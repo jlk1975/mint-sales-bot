30e8171ef4c731851826dec51e71c6b018b804aa
jlk <jkendall1975@gmail.com>
2023-01-14 16:28:03 -0500

    mints 2

---
+++ watch-for-mints.js
@@ -0,0 +1,29 @@
+// npm install @alchemy-sdk
+import { Network, Alchemy } from "alchemy-sdk";
+
+const settings = {
+  apiKey: API_KEY, // Replace with your Alchemy API Key.
+  network: Network.MATIC_MAINNET, // Replace with your network.
+};
+
+const alchemy = new Alchemy(settings);
+
+// This is the "transfer event" topic we want to watch.
+const mintTopic = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+// This is the "from address" we want to watch.
+const zeroTopic =
+  "0x0000000000000000000000000000000000000000000000000000000000000000";
+// This is the NFT contract we want to watch.
+const nftContractAddress = "0x029Dfd5B016ed918416CD1fAF53d79A013b3065c";
+
+// Create the log options object.
+const hackrDaoMintEvents = {
+  address: nftContractAddress,
+  topics: [mintTopic, zeroTopic],
+};
+
+// TODO: Add whatever logic you want to run upon mint events.
+const doSomethingWithTxn = (txn) => console.log(txn);
+
+// Open the websocket and listen for events!
+alchemy.ws.on(hackrDaoMintEvents, doSomethingWithTxn);
