7a73cd7ce23e5fa7c96e63a00bc488ae6c229ae4
jlk <jkendall1975@gmail.com>
2023-01-14 19:25:48 -0500

    mints 5

--- watch-for-mints.js
+++ watch-for-mints.js
@@ -1,11 +1,13 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 const MATIC_KEY = process.env.ALCHEMY_MATIC_MAINNET;
+const GORELI_KEY = process.env.ALCHEMY_ETH_GORELI;
+
 import { Network, Alchemy } from "alchemy-sdk";
 
 const settings = {
-  apiKey: MATIC_KEY,  
-  network: Network.MATIC_MAINNET,  
+  apiKey: GORELI_KEY,  
+  network: Network.ETH_GOERLI,  
 };
 
 const alchemy = new Alchemy(settings);
@@ -19,7 +21,7 @@ const zeroTopic =
 const nftContractAddress = "0x029Dfd5B016ed918416CD1fAF53d79A013b3065c";
 
 // Create the log options object.
-const hackrDaoMintEvents = {
+const PixaMintEvents = {
   address: nftContractAddress,
   topics: [mintTopic, zeroTopic],
 };
@@ -28,4 +30,11 @@ const hackrDaoMintEvents = {
 const doSomethingWithTxn = (txn) => console.log(txn);
 
 // Open the websocket and listen for events!
-alchemy.ws.on(hackrDaoMintEvents, doSomethingWithTxn);
+alchemy.ws.on(PixaMintEvents, doSomethingWithTxn);
+
+// Example
+// New Mint! The Snowflake Badge -> This is a Property called "TYPE"
+// Minted By: 0x392
+// Collection: PixaBadges
+// Link: https://opensea.io/assets/matic/0x029dfd5b016ed918416cd1faf53d79a013b3065c/3039
+// Can we get the tokenid from the txn hash?
